e9b753b05eebb9d12fcfbdbe1ba6978318710326
RohanS14 <rohan@subramanianfamily.com>
2023-11-20 01:02:08 -0800

    add test

--- test_most_active_cookie.py
+++ test_most_active_cookie.py
@@ -1,18 +1,4 @@
 #! /usr/bin/env python3
 
-import sys, os
+import pytest
 
-# add current working directory to path
-sys.path.insert(0, os.getcwd())
-
-import most_active_cookie
-import unittest
-
-class CookieTests(unittest.TestCase):
-    def 
-
-def main():
-    return unittest.main()
-
-if __name__ == '__main__':
-    sys.exit(main())
